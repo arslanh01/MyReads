c2558844f1265dcf1ef0b6d4b36ec6f6ed626772
arslanh01 <ah644635@gmail.com>
2022-07-06 15:23:54 +0500

    fix: Fix Read.js

--- starter/src/components/Read.js
+++ starter/src/components/Read.js
@@ -5,7 +5,11 @@ const Read = ({ readBooks }) => {
     <div className="bookshelf">
       <h2 className="bookshelf-title">Read</h2>
       <div className="bookshelf-books">
-        <ol className="books-grid">{readBooks}</ol>
+        <ol className="books-grid">
+          {readBooks.map((book) => {
+            return <Book key={book.id} book={book} />;
+          })}
+        </ol>
       </div>
     </div>
   );
