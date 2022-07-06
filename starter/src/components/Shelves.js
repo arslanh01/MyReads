8d5a50376f5a28ac365f297fc558ad63e77b8f35
arslanh01 <ah644635@gmail.com>
2022-07-06 11:03:51 +0500

    refactor: Separate each component

--- starter/src/components/Shelves.js
+++ starter/src/components/Shelves.js
@@ -1,1 +1,25 @@
+import CurrentlyReading from "./CurrentlyReading";
+import WantToRead from "./WantToRead";
+import Read from "./Read";
 
+const Shelves = () => {
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          <CurrentlyReading />
+          <WantToRead />
+          <Read />
+        </div>
+      </div>
+      <div className="open-search">
+        <a>Add a book</a>
+      </div>
+    </div>
+  );
+};
+
+export default Shelves;
