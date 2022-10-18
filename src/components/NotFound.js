516f061f8b29f93d6382cb7b43fc3f7c60e403d8
Arslan Haider <75937922+arslanh01@users.noreply.github.com>
2022-10-18 17:01:42 +0500

    Update NotFound.js

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
     <div>
-      <Link to="/">Go back to home</Link>
+      <Link to="MyReads/">Go back to home</Link>
       <h3>Oops! Looks like the page doesn't exist.</h3>
     </div>
   );
